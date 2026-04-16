aaa23cab4c58fe836db80f9898fca6ab48fd71da
OrkVili <n7n1mg@uni-sopron.hu>
2026-04-16 14:03:58 +0200

    Data context

---
+++ SzereloMuhely/SzereloMuhely/Data/ServiceContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SzereloMuhely.Data
+{
+    public class ServiceContext:DbContext
+    {
+        public DbSet<Models.User> Users{ get; set; } = null!;
+        public DbSet<Models.Mechanic> Mechanics { get; set; } = null!;
+        public DbSet<Models.WorkRecorder> WorkRecorders { get; set; } = null!;
+        public DbSet<Models.WorkSheet> WorkSheets { get; set; } = null!;
+        public DbSet<Models.WorkProcess> WorkProcesses { get; set; } = null!;
+        public DbSet<Models.Material> Materials { get; set; } = null!;
+        public DbSet<Models.Part> Parts { get; set; } = null!;
+        public DbSet<Models.Vehicle> Vehicles { get; set; } = null!;
+
+        public ServiceContext(DbContextOptions<ServiceContext> options) : base(options)
+        {
+        }
+    }
+}
